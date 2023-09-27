df0869560608c2c31edd35b65eeb1192c9a4610c
Carlos Daniel <dani99yagami33@gmail.com>
2023-09-27 16:07:26 -0300

    Apply Ia

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -11,34 +11,40 @@ document.getElementById('cep').addEventListener('blur', (e) => {
 })
 
 function retireHifem(cepValue) {
-    let string = cepValue
+    let string = cepValue;
+    // Check if the CEP value contains a hyphen
     if (string.includes("-")) {
         string = string.replace("-", "");
+        // Check if the resulting string is a valid number
         if (!isNaN(string)) {
-            return string
+            return string;
         } else {
-            alert("Digite um cep valido !!")
+            alert("Digite um cep valido !!");
         }
     } else {
-        alert("Digite um cep valido !!")
+        alert("Digite um cep valido !!");
     }
 }
 
 async function searchForCep(cepNumber) {
     try {
+        // Send a request to the CEP API
         let response = await fetch(`https://viacep.com.br/ws/${cepNumber}/json/`);
         let data = await response.json();
-        console.log(data);
+        console.log('Data retrieved:', data);
         if (!data.erro) {
+            // Update the input fields with the address information
             document.getElementById('rua').value = data.logradouro;
             document.getElementById('bairro').value = data.bairro;
             document.getElementById('cidade').value = data.localidade;
             document.getElementById('estado').value = data.uf;
             document.getElementById('btn').disabled = false;
         } else {
+            // Display an alert if the CEP is not found
             alert('CEP não encontrado.');
         }
     } catch {
+        // Display an alert if there's an error while querying the CEP
         alert('Erro ao consultar o CEP.');
     }
 }
